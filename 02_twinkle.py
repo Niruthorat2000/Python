e6d32e694bdefaf9966d354234564696546b37a6
Niruthorat2000 <niranjanthorat0403@gmail.com>
2024-08-09 23:13:12 +0530

    Add files via upload

---
+++ 02_twinkle.py
@@ -0,0 +1,11 @@
+# '# ' it is used for single line comment and
+# ''' it is used for multiline coment '''
+# coment is used to write the text which we dont want to excute .
+
+print( '''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.''')
+
+print( '''it is a build level testing technique during this testing test engineer validate  that internal functionalty depends on external iterface.
+ it is also called as system and functional testing ''')
